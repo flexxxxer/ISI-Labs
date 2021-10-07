201d8a961d23df783b9d35b5ac8ce5bc81a2ccad
flexxxer <flexxxer@LAPTOP>
2021-10-07 19:45:07 +0200

    Add project files.

---
+++ BasedFunctionalFeatures/Pipeline.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BasedFunctionalFeatures
+{
+    public static class Pipeline
+    {
+        public static TOut As<TIn, TOut>(this TIn self, Func<TIn, TOut> @as)
+            => @as(self);
+    }
+}
---
+++ BasedFunctionalFeatures/StringExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BasedFunctionalFeatures
+{
+    public static class StringExt
+    {
+        public static string JoinBy<T>(this IEnumerable<T> sequence, string separator)
+            => string.Join(separator, sequence);
+
+        public static string JoinBy<T>(this IEnumerable<T> sequence, char separator)
+            => string.Join(separator, sequence);
+    }
+}
